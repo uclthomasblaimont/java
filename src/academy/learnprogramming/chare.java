9a20326ed5b0547948c3d3853820fce3ba8f81f1
uclthomasblaimont <94004123+uclthomasblaimont@users.noreply.github.com>
2022-07-10 15:36:04 +0200

    java test 1

--- src/academy/learnprogramming/chare.java
+++ src/academy/learnprogramming/chare.java
@@ -3,7 +3,26 @@ package academy.learnprogramming;
 public class chare {
     public static void main(String[] args){
         char myChar='D';
+        System.out.println(myChar);
+        char myUnicodeChar ='\u00A9';
+        System.out.println(myUnicodeChar);
 
+
+         boolean myTrueBooleanValue = true;
+         boolean myFalseBooleanValue = false;
+
+         boolean isCustomerOverTwentyOne=true;
+
+         //backslash c est pour les commentaires
+        String myString ="This is a string";
+        System.out.println("my string is equal to "+ myString);
+        myString= myString +" and this is more .";
+        System.out.println("my string is equal to "+ myString);
+
+        String mystringnumber="250.60";
+        mystringnumber=mystringnumber+"50.35";
+        //concatenation with the other string "50.35"
+        System.out.println(mystringnumber);
     }
 
 }
